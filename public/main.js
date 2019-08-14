dc6f75d806e02e2d9aed825deabb6dcc91626153
ads6495 <astrange55@icloud.com>
2019-08-13 21:48:26 -0400

    added main functionality

--- public/main.js
+++ public/main.js
@@ -1,7 +1,25 @@
-const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
-}
+let city = document.querySelector('#typedCity')
 
-document.addEventListener('DOMContentLoaded', main)
+
+const createProperSearch = () => {
+  const typedCity = city.value
+  const url = 'https://api.openweathermap.org/data/2.5/weather'
+  const apiKey = '6ea45fb71cd9bf02b74216acfd6fb476'
+  let search = typedCity
+  const newUrl = url + '?' + 'q=' + search + '&appid=' + apiKey
+  console.log(newUrl)
+  return newUrl
+}
+const getWeatherAndTemp = () => {
+  fetch(createProperSearch())
+    .then(resp => {
+      return resp.json()
+    })
+    .then(city => {
+      console.log(city)
+      document.querySelector('.weather').textContent = city.weather[0].main
+      document.querySelector('.city').textContent = city.main.temp
+    })
+
+}
+document.querySelector('.search').addEventListener('click', getWeatherAndTemp)
